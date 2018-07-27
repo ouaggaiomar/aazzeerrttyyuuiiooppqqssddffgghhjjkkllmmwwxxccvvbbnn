c7b1dbc5181adb7a2a555abc7ca4db13246351b3
ouaggaiomar <41638862+ouaggaiomar@users.noreply.github.com>
2018-07-27 19:53:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,33 +198,7 @@ client.on("guildMemberAdd", (member) => {
 
      }
        });
-  
-Save New Duplicate & Edit Just Text Twitter
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 //By OMAR#0616 //By OMAR#0616 
