54a1a307a0bcc7fdfa8d10a82c48db5c625b819f
ouaggaiomar <41638862+ouaggaiomar@users.noreply.github.com>
2018-07-29 00:34:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,8 +55,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('£bcall')){
- if (message.author.id !== 'هنا') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === 'هنا') return;
+ if (message.author.id !== '418705499031076864') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '418705499031076864') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
