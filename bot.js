5e2e7fcc49afb72bdd9de54f397c9a6a8bcd0f22
ouaggaiomar <41638862+ouaggaiomar@users.noreply.github.com>
2018-07-29 20:18:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,7 +191,8 @@ client.on('message', message => {
     .setFooter(`Morro bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
-}       });
+}       
+});
  
 
 
