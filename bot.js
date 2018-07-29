4aa8d262c09b60bbf3ea779f2272eca562fdb3b0
ouaggaiomar <41638862+ouaggaiomar@users.noreply.github.com>
2018-07-29 20:08:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,19 +163,7 @@ client.on('message', msg => {
                   }
 });
 
- client.on('message', message => {
-            if (message.content.startsWith(prefix + "bot")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
-.addField(' الاعضاء👥 ',` [${client.users.size}] `)
-.addField('الرومات📚 ',`[${client.channels.size}]`) 
-.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
-.addField('** Games Station Arabic ♥`)
-.setColor('#7d2dbe')
-  message.channel.sendEmbed(embed);
-    }
-});
+ 
 
 
 
